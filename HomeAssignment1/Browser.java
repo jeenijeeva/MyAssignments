18d357252292a9865cd713a863dfcc9c0b45a7db
jeenijeeva <159539052+jeenijeeva@users.noreply.github.com>
2024-02-09 17:28:47 +0530

    Add files via upload
    Home assignments 1

---
+++ HomeAssignment1/Browser.java
@@ -0,0 +1,17 @@
+package HomeAssignment1;
+
+public class Browser {
+
+	public static void main(String[] args) {
+
+		float browserVersion = 100.2F;
+		String browserName = "Chrome";
+		boolean pageLoaded = true;
+		int noOfBrowser = 2000;
+		char browserLogo = 'W';
+		
+		 System.out.println("Browser Version: "+ browserVersion + "\n" + "Browser Name: "+ browserName + "\n" +"PageLoaded: "+ pageLoaded +"\n"+ "No of Browser: "+  noOfBrowser + "\n" +"Browser Logo: "+ browserLogo);
+		
+	}
+
+}
